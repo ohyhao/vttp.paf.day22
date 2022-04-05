4783de55916ab926e15df75e147c08ef1e1cc2f6
ohyhao <ohyonghao@outlook.com>
2022-04-05 16:31:21 +0800

    added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# My first workflow on GitHub
+
+[![Complie, test, deploy](https://github.com/ohyhao/vttp.paf.day22/actions/workflows/main.yaml/badge.svg)](https://github.com/ohyhao/vttp.paf.day22/actions/workflows/main.yaml)
+
+![Coverage](.github/badges/jacoco.svg)
--- src/test/java/vttp2022/paf/day22/Day22ApplicationTests.java
+++ src/test/java/vttp2022/paf/day22/Day22ApplicationTests.java
@@ -1,13 +1,25 @@
 package vttp2022.paf.day22;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import vttp2022.paf.day22.services.GiphyService;
 
 @SpringBootTest
 class Day22ApplicationTests {
 
+	@Autowired
+	private GiphyService giphySvc;
+
 	@Test
-	void contextLoads() {
+	void shouldLoad10Images() {
+		List<String> result = giphySvc.getGiphs("dog");
+		assertEquals(10, result.size(), "Default number of gifs");
 	}
 
 }
